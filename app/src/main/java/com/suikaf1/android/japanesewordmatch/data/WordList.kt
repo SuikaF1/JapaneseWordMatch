b0731f6c16032c59cffc264d6f08306c2842b3c1
Steven Coffee <Frost-Tech128@users.noreply.github.com>
2021-10-28 11:39:39 -0700

    I updated the strings file to include the words that will be unscrambled I also created a list of the string resource ids to be able to reference them in my code later from a list.

--- app/src/main/java/com/suikaf1/android/japanesewordmatch/data/WordList.kt
+++ app/src/main/java/com/suikaf1/android/japanesewordmatch/data/WordList.kt
@@ -1,6 +1,19 @@
 package com.suikaf1.android.japanesewordmatch.data
 
-class WordList
-{
+import com.suikaf1.android.japanesewordmatch.R
 
-}
+const val NUMBER_OF_WORDS = 11
+
+val wordsList: List<Int> =
+    listOf(
+    R.string.pencil,
+    R.string.umbrella,
+    R.string.bag,
+    R.string.shoes,
+    R.string.wallet,
+    R.string.jeans,
+    R.string.dictionary,
+    R.string.bicycle,
+    R.string.newspaper,
+    R.string.tshirt,
+    R.string.watch_clock)
